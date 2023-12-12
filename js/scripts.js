a2ac1ce5029b0d4bf279065998c37bcca830c05a
lpitto86 <luca.p481@gmail.com>
2023-12-12 15:57:55 +0100

    Pulsanti

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,41 @@ function showImage(index) {
 setTimeout(() => {
     showImage(1);
 }, 2000);
+
+// Funzione per cambiare l'immagine visibile
+function showImage(index) {
+      // Rimuovi la classe 'visible' da tutti gli elementi
+      items.forEach(item => item.classList.remove('visible'));
+  
+      // Aggiungi la classe 'visible' all'elemento desiderato
+      items[index].classList.add('visible');
+  }
+  
+  // Gestione dell'evento click sulla freccia sinistra
+  document.getElementById('arrow-left').addEventListener('click', function () {
+      // Trova l'indice dell'immagine attualmente visibile
+      const currentIndex = Array.from(items).findIndex(item => item.classList.contains('visible'));
+  
+      // Calcola l'indice dell'immagine precedente
+      const previousIndex = (currentIndex - 1 + items.length) % items.length;
+  
+      // Mostra l'immagine precedente
+      showImage(previousIndex);
+  });
+  
+  // Gestione dell'evento click sulla freccia destra
+  document.getElementById('arrow-right').addEventListener('click', function () {
+      // Trova l'indice dell'immagine attualmente visibile
+      const currentIndex = Array.from(items).findIndex(item => item.classList.contains('visible'));
+  
+      // Calcola l'indice dell'immagine successiva
+      const nextIndex = (currentIndex + 1) % items.length;
+  
+      // Mostra l'immagine successiva
+      showImage(nextIndex);
+  });
+  
+  // Esempio: Mostra la seconda immagine dopo 2 secondi
+  setTimeout(() => {
+      showImage(1);
+  }, 2000);
