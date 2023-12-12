2261a48a168629b2ad7c084dbc8d2f4444cb862d
lpitto86 <luca.p481@gmail.com>
2023-12-12 15:39:19 +0100

    Immagini

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+const images = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg'];
+
+// Ottieni il riferimento all'elemento del carosello
+const carousel = document.getElementById('carousel');
+
+// Ciclo per creare le immagini dinamicamente e aggiungerle al carosello
+for (let i = 0; i < images.length; i++) {
+    // Creazione di un elemento div per ogni immagine
+    const item = document.createElement('div');
+    item.className = 'carousel-item';
+
+    // Creazione di un elemento img per l'immagine
+    const img = document.createElement('img');
+    img.className = 'carousel-img';
+    img.src = images[i];
+    img.alt = `Image ${i + 1}`;
+
+    // Aggiungi l'immagine al div
+    item.appendChild(img);
+
+    // Aggiungi il div al carosello
+    carousel.appendChild(item);
+}
+
+// Ottieni la lista di tutti gli elementi del carosello
+const items = document.querySelectorAll('.carousel-item');
+
+// Inizialmente mostra solo la prima immagine
+items[0].classList.add('visible');
+
+// Funzione per cambiare l'immagine visibile
+function showImage(index) {
+    // Rimuovi la classe 'visible' da tutti gli elementi
+    items.forEach(item => item.classList.remove('visible'));
+
+    // Aggiungi la classe 'visible' all'elemento desiderato
+    items[index].classList.add('visible');
+}
+
+// Esempio: Mostra la seconda immagine dopo 2 secondi
+setTimeout(() => {
+    showImage(1);
+}, 2000);
